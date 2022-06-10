fd3eeea0a61e23074d2292d3ababa9e025f7a858
Madhu reddy <ashokit8580@gmail.com>
2022-06-10 15:40:14 +0530

    added git ignore file

--- src/main/java/in/ashokit/security/service/PasswordService.java
+++ src/main/java/in/ashokit/security/service/PasswordService.java
@@ -6,16 +6,16 @@ import java.util.Base64.Encoder;
 
 public class PasswordService {
 
-	public String encrypt(String text) {
+	public String encrypt(String plainText) {
 		Encoder encoder = Base64.getEncoder();
-		String encodedString = encoder.encodeToString(text.getBytes());
+		String encodedString = encoder.encodeToString(plainText.getBytes());
 		return encodedString;
 	}
 
-	public String decrypt(String encryptedText) {
+	public String decrypt(String encodedText) {
 
 		Decoder decoder = Base64.getDecoder();
-		byte[] decode = decoder.decode(encryptedText);
+		byte[] decode = decoder.decode(encodedText);
 		return new String(decode);
 	}
 
